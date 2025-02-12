4436602f206e34ce6d0699e408eb771a61b87960
SosinaTefera <diyanatefera123@gmail.com>
2025-02-12 12:15:31 +0300

    user routes regisetered in the main file

--- app/main.py
+++ app/main.py
@@ -1,11 +1,16 @@
 from fastapi import FastAPI
+from app.services.database import Base, engine
+from app.routes import user
 
 app = FastAPI()
+
+# Create database tables
+Base.metadata.create_all(bind=engine)
+
+app.include_router(user.router, prefix="/user", tags=["User"])
+
+
 
 @app.get("/")
 def read_root():
     return {"message": "Welcome to the Tour Guide API"}
-
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
