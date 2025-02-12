ced4474f40db5e46853e28c9086bd0478feaa0c0
SosinaTefera <diyanatefera123@gmail.com>
2025-02-12 11:41:33 +0300

    added methods to hash password, verify password, create access token and to get current user from access token inside auth.py

---
+++ app/services/auth.py
@@ -0,0 +1,30 @@
+from passlib.context import CryptContext
+from datetime import datetime, timedelta
+import jwt
+from app.config import SECRET_KEY, ALGORITHM, ACCESS_TOKEN_EXPIRE_MINUTES
+from fastapi import Depends, HTTPException
+from fastapi.security import OAuth2PasswordBearer
+
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+def hash_password(password: str) -> str:
+    return pwd_context.hash(password)
+
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    return pwd_context.verify(plain_password, hashed_password)
+
+def create_access_token(data: dict):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+
+def get_current_user(token: str = Depends(oauth2_scheme)):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=["HS256"])
+        return {"email": payload["sub"], "role": payload["role"],"id": payload['id']}
+    except :
+        raise HTTPException(status_code=401, detail="Invalid token")
