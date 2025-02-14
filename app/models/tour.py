6628a7c5cbc6e881daae4b4a12b510de43ba956d
SosinaTefera <diyanatefera123@gmail.com>
2025-02-14 23:53:45 +0300

    tour model added

---
+++ app/models/tour.py
@@ -0,0 +1,17 @@
+from sqlalchemy import Column, String, Text, Float, DateTime
+from app.services.database import Base
+
+
+
+class Tour(Base):
+    __tablename__ = "tours"
+
+    id = Column(String, primary_key=True)
+    title = Column(String, nullable=False)
+    description = Column(Text, nullable=False)
+    latitude = Column(Float, nullable=False)
+    longitude = Column(Float, nullable=False)
+    image = Column(String, nullable=True)  
+    price = Column(Float, nullable=False)
+    created_at = Column(DateTime) 
+
