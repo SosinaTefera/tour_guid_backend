a7c5edaf1d778ccc4dfa26e920c242f81e2aa921
SosinaTefera <diyanatefera123@gmail.com>
2025-02-20 12:31:39 +0300

    user profile get and update endpoints added

--- app/routes/user.py
+++ app/routes/user.py
@@ -2,9 +2,10 @@ from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from app.services.database import get_db
 from app.models.user import User
-from app.schemas import UserCreate, UserLogin
-from app.services.auth import hash_password, verify_password, create_access_token
+from app.schemas import UserCreate, UserLogin, UserProfileResponse, UserProfileUpdate
+from app.services.auth import hash_password, verify_password, create_access_token, get_current_user
 import uuid
+
 
 router = APIRouter()
 
@@ -33,8 +34,32 @@ def login(user: UserLogin, db: Session = Depends(get_db)):
     if not db_user or not verify_password(user.password, db_user.password):
         raise HTTPException(status_code=401, detail="Invalid email or password")
 
-    access_token = create_access_token({"sub": db_user.email, "role": db_user.role, "id":db_user.id})
+    access_token = create_access_token({"sub": db_user.email, "role": db_user.role, "id":db_user.id,"first_name":db_user.first_name, "last_name":db_user.last_name})
     return {"access_token": access_token, "token_type": "bearer"}
 
 
+@router.get("/profile", response_model=UserProfileResponse)
+def get_profile(current_user: User = Depends(get_current_user),db: Session = Depends(get_db),):
+    user_id = current_user['id']
+    user = db.query(User).filter(User.id == user_id).first()
 
+    return user
+
+
+@router.put("/profile", response_model=UserProfileResponse)
+def update_profile(
+    update_data: UserProfileUpdate, 
+    db: Session = Depends(get_db), 
+    current_user: User = Depends(get_current_user)
+):
+    user_id = current_user['id']
+    user = db.query(User).filter(User.id == user_id).first()
+    if update_data.first_name:
+        user.first_name = update_data.first_name
+    if update_data.last_name:
+        user.last_name = update_data.last_name
+   
+    db.commit()
+    db.refresh(user)
+    
+    return user
--- app/services/auth.py
+++ app/services/auth.py
@@ -25,6 +25,6 @@ def create_access_token(data: dict):
 def get_current_user(token: str = Depends(oauth2_scheme)):
     try:
         payload = jwt.decode(token, SECRET_KEY, algorithms=["HS256"])
-        return {"email": payload["sub"], "role": payload["role"],"id": payload['id']}
+        return {"email": payload["sub"], "role": payload["role"],"id": payload['id'], "first_name": payload['first_name'], "last_name": payload['last_name']}
     except :
         raise HTTPException(status_code=401, detail="Invalid token")
